41a27868df64628ffdc1d39a92a94fb547941745
Sam Fernandez <fernandez.sjackson@gmail.com>
2025-08-29 16:07:59 +0800

    added password reset request and update endpoints, url redirects from email to follow

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
-  
+  respond_to :json
+
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    if successfully_sent?(resource)
+      render json: { message: "Reset instructions sent to #{resource.email}" }, status: :ok
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    if resource.errors.empty?
+      render json: { message: "Password successfully updated" }, status: :ok
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # private
+
+  # def resource_params
+  #   params.require(:user).permit(:email, :password, :password_confirmation, :reset_password_token)
+  # end
+
   # GET /resource/password/new
   # def new
   #   super
